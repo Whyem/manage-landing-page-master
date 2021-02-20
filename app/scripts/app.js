2759bfe4cbf104aad4d6b70a56a4a29cdcc2cdf7
Whyem <yassirmerimi@gmail.com>
2021-02-20 04:40:55 +0100

    3rd commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
         focus: 'center',
         autoplay: true,
     } ).mount();
+
+    $(".hamburger").click(function(){
+        $(this).toggleClass("closed");
+        $(".overlay").toggleClass("fade-in");
+        $(".header__links__mobile").toggleClass("fade-in clickable");
+        $("body").toggleClass("noscroll");
+    });
 });
